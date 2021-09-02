da0ba1b2fd4f6eae36e08a6534d0031077551d73
Sam Adams <samharad@umich.edu>
2021-09-02 19:36:48 -0400

    figured out the pREPL thing

--- src/kinder/intellij.clj
+++ src/kinder/intellij.clj
@@ -1,24 +1,56 @@
-(ns kinder.intellij
-  (:require [tubular.core :as tubular]
-            []))
+(ns kinder.intellij)
+  ;(:require [tubular.core :as tubular]))
 
 (comment
   "So what I basically need here:
+  - User needs to have enabled and started the IDE REPL.
+  - Then, my library sends over the form required for starting a
+    PREPL on a NEW port:"
+  (start-server {:accept 'clojure.core.server/io-prepl
+                 :address "127.0.0.1"
+                 :port 40001
+                 :name "prepl"})
+
+  "- Then, we establish a pREPL connection to it using:"
+  (remote-prepl "localhost" 40001 *in* println)
+
+  " But with different in/out streams. The in stream is a buffer
+  to which we write; the out-fn handles exceptions or, hopefully,
+  proper return values.
+
+  Note that something weird happens when the IDE pREPL returns a tagged
+  literal like #object['foobar'] -- something complains about not having
+  a tagged literal handler for it.
+  "
+
+  "
   - A means of upgrading the IDE socket repl to a
     p-style-REPL, possibly Unrepl.
   - A Clojure client for the upgraded REPL.
   - Functions for working with IntelliJ.
+
+  Of course, to start I could just fire commands to
+  the IDE REPL, and hope they work.
+
+  BUT I need to think about the use case more first,
+  for sure. Currently I have none. Because if my plan
+  is to programmatically modify a source file, that's
+  a little silly, since unsaved edits might be present
+  in the IntelliJ (or emacs) buffer. It's overall a
+  silly task that prolly needs abandoned.
+
   ")
 
 
-(defn foo []
-  (binding [*in* (clojure.java.io/reader
-                   (char-array
-                     (str "(+ 1 1)" \newline \return
-                          "(flush)" \newline \return
-                          "(java.lang.Thread/sleep 1000)" \newline \return)))
-            *out* (java.io.StringWriter.)]
-    (tubular/connect 40000)
-    (.toString *out*)))
+#_(defn foo []
+    (binding [*in* (clojure.java.io/reader
+                     (char-array
+                       (str "(+ 1 1)" \newline \return
+                            "(flush)" \newline \return
+                            "(java.lang.Thread/sleep 1000)" \newline \return)))
+              *out* (java.io.StringWriter.)]
+      (tubular/connect 40000)
+      (.toString *out*)))
 
-(foo)
+(.syncRefresh
+  (com.intellij.openapi.vfs.VirtualFileManager/getInstance))
