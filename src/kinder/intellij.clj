a28904d12e2e595a20f2283e9ac56edf05d2ea08
Sam Adams <samharad@umich.edu>
2021-09-08 18:09:58 -0400

    established ide-repl communication via piped-reader/writer

--- src/kinder/intellij.clj
+++ src/kinder/intellij.clj
@@ -1,20 +1,56 @@
-(ns kinder.intellij)
-  ;(:require [tubular.core :as tubular]))
+(ns kinder.intellij
+  (:require [tubular.core :as tubular]
+            [clojure.java.io :refer [reader]]
+            [clojure.core.server :as server]
+            [clojure.core.async :refer [go]])
+  (:import (java.io PipedWriter PipedReader)))
+
+(def piped-writer (PipedWriter.))
+(def piped-reader (PipedReader. piped-writer))
+
+
+(defn start-ide-prepl! [existing-ide-repl-port ide-prepl-port]
+  (binding [*in* (reader
+                   (char-array
+                     (str "(clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl\n                 :address \"127.0.0.1\"\n                 :port " ide-prepl-port "\n                 :name \"prepl\"})\n:repl/quit\n")))
+            *out* (java.io.StringWriter.)]
+    (tubular/connect existing-ide-repl-port)))
+
+(defn connect-to-ide-prepl [port]
+  (go
+    (server/remote-prepl "localhost"
+                         port
+                         piped-reader
+                         println)))
+
+(defn send-form [form]
+  (do
+    (.write piped-writer (str form "\n"))
+    (.flush piped-writer)))
+
+(defn init! []
+  (start-ide-prepl! 40000 40001)
+  (connect-to-ide-prepl 40001))
 
 (comment
-  "So what I basically need here:
+  (init!)
+  (send-form "(.toString (com.intellij.openapi.vfs.VirtualFileManager/getInstance))")
+  (send-form "(+ 3 3)"))
+
+#_(comment
+    "So what I basically need here:
   - User needs to have enabled and started the IDE REPL.
   - Then, my library sends over the form required for starting a
     PREPL on a NEW port:"
-  (start-server {:accept 'clojure.core.server/io-prepl
-                 :address "127.0.0.1"
-                 :port 40001
-                 :name "prepl"})
+    (start-server {:accept 'clojure.core.server/io-prepl
+                   :address "127.0.0.1"
+                   :port 40001
+                   :name "prepl"})
 
-  "- Then, we establish a pREPL connection to it using:"
-  (remote-prepl "localhost" 40001 *in* println)
+    "- Then, we establish a pREPL connection to it using:"
+    (remote-prepl "localhost" 40001 *in* println)
 
-  " But with different in/out streams. The in stream is a buffer
+    " But with different in/out streams. The in stream is a buffer
   to which we write; the out-fn handles exceptions or, hopefully,
   proper return values.
 
@@ -23,7 +59,7 @@
   a tagged literal handler for it.
   "
 
-  "
+    "
   - A means of upgrading the IDE socket repl to a
     p-style-REPL, possibly Unrepl.
   - A Clojure client for the upgraded REPL.
@@ -52,5 +88,5 @@
       (tubular/connect 40000)
       (.toString *out*)))
 
-(.syncRefresh
-  (com.intellij.openapi.vfs.VirtualFileManager/getInstance))
+#_(.syncRefresh
+    (com.intellij.openapi.vfs.VirtualFileManager/getInstance))
